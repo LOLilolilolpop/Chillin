0d7f13ca0c85f7971df5c18f93085c25949a4031
haoyu.choong@repromarketing.com <haoyu.choong@repromarketing.com>
2024-01-17 03:03:42 +0800

    responsive function done

---
+++ Chillin/Pages/craving.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chillin.Pages
+{
+    public class cravingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
