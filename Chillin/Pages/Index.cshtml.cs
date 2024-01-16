656d21c0fb37f815c9101e2408ff87b80f77cf61
LOLilolilolpop <87427912+LOLilolilolpop@users.noreply.github.com>
2024-01-16 16:58:10 +0800

    Typed h1 and h2

--- Chillin/Pages/Index.cshtml.cs
+++ Chillin/Pages/Index.cshtml.cs
@@ -5,16 +5,8 @@ namespace Chillin.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
         }
     }
 }
