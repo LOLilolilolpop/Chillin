4b58d558d3dfeadd4d5959bc028734a78acf474f
haoyu.choong@repromarketing.com <haoyu.choong@repromarketing.com>
2024-03-01 17:35:05 +0800

    -craving page stucked -music page started

---
+++ Chillin/Pages/music.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chillin.Pages
+{
+    public class musicModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
